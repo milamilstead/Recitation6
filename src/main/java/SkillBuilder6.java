9a7cead5c4cd040dd2ce6bdcd1214fed542888c5
milamilstead <mila.milstead@gmail.com>
2025-04-13 23:46:58 -0400

    complete

--- src/main/java/SkillBuilder6.java
+++ src/main/java/SkillBuilder6.java
@@ -5,21 +5,19 @@
  * @author (you)
  * @version (a version number or a date)
  */
-public class SkillBuilder6
-{
+public class SkillBuilder6 {
     /**
      * Calculates the prefix average of array data and returns the prefex average
      * in a new array.  The parameter data is never touched and left intact.
      *
      * @param data array of double values on which to calculate the prefix average.
      * @return returns an array containing the prefix average values calculated
-     *         from data.
+     * from data.
      */
-    public static double[] prefixAverage(double[] data)
-    {
+    public static double[] prefixAverage(double[] data) {
         double[] result = new double[data.length];
         double sum = 0;
-        for(int i = 0; i< data.length; i++){
+        for (int i = 0; i < data.length; i++) {
             sum += data[i];
             result[i] = sum / (i + 1);
         }
@@ -30,14 +28,14 @@ public class SkillBuilder6
      * Finds the location in array anArray where value is located.  If anArray
      * does not contain an element equal to value a -1 is returned; otherwise a
      * positive or zero index value is returned.
+     *
      * @param searchValue value to look for in the array
-     * @param anArray array in which to look for a value
+     * @param anArray     array in which to look for a value
      * @returns index of the value in the array; -1 otherwise.
      */
-    public static int indexOf(int searchValue, int[] anArray)
-    {
-        for(int i = 0; i < anArray.length; i++){
-            if (anArray[i] == searchValue){
+    public static int indexOf(int searchValue, int[] anArray) {
+        for (int i = 0; i < anArray.length; i++) {
+            if (anArray[i] == searchValue) {
                 return i;
             }
         }
@@ -48,14 +46,14 @@ public class SkillBuilder6
      * Finds the location in array anArray where string s is located.  If anArray
      * does not contain an element equal to s a -1 is returned; otherwise a
      * positive or zero index value is returned.
-     * @param s a string to look for in the array
+     *
+     * @param s       a string to look for in the array
      * @param anArray array in which to look for a value
      * @returns index of the string s in the array; -1 otherwise.
      */
-    public static int indexOf(String s, String[] anArray)
-    {
-        for (int i = 0; i < anArray.length; i++){
-            if(anArray[i].equals(s)){
+    public static int indexOf(String s, String[] anArray) {
+        for (int i = 0; i < anArray.length; i++) {
+            if (anArray[i].equals(s)) {
                 return i;
             }
         }
@@ -65,13 +63,19 @@ public class SkillBuilder6
     /**
      * Finds all occurrence of string s in anArray and removes them, returning
      * a new array with all occurrences of s removed.
-     * @param s string to search for in array
+     *
+     * @param s       string to search for in array
      * @param anArray array in which to search and remove s
      * @return An array with all occurrences of s removed.
      */
-    public static String[] remove(String s, String[] anArray)
-    {
-        String[] newArray = new String[anArray.length - 1];
+    public static String[] remove(String s, String[] anArray) {
+        int count = 0;
+        for (String string : anArray) {
+            if (!string.equals(s)) {
+                count++;
+            }
+        }
+        String[] newArray = new String[count];
         int index = 0;
 
         for (String string : anArray) {
@@ -85,12 +89,20 @@ public class SkillBuilder6
 
     /**
      * Reverses an array of integers.
+     *
      * @param anArray the array whose contents should be reversed.
      */
-    public static void reverse(int[] anArray)
-    {
-        for (int i = anArray.length -1 ; i >= 0; i--){
-            System.out.print(anArray[i]);
+    public static void reverse(int[] anArray) {
+        int side1 = 0;
+        int side2 = 0;
+
+        while (side1 < side2) {
+            int temp = anArray[side1];
+            anArray[side1] = anArray[side2];
+            anArray[side2] = temp;
+
+            side1++;
+            side2--;
         }
     }
 }
